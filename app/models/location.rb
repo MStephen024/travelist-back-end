6bb169ad8114f5b92147637d722fc9609aa488e1
MStephen024 <s.melendez024@gmail.com>
2019-07-24 19:15:26 -0400

    Added User Macros to Models and Serializer
    Updated models and serializer files

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,2 +1,7 @@
+# frozen_string_literal: true
+
 class Location < ApplicationRecord
+  belongs_to :user
+
+  validates :text, :user, presence: true
 end
--- app/serializers/location_serializer.rb
+++ app/serializers/location_serializer.rb
@@ -1,3 +1,12 @@
+# frozen_string_literal: true
+
 class LocationSerializer < ActiveModel::Serializer
-  attributes :id, :country, :city, :attraction, :cuisine, :description
+  attributes :id, :country, :city, :attraction, :cuisine, :description,
+             :editable
+
+  belongs_to :user
+
+  def editable
+    scope == object.user
+  end
 end
